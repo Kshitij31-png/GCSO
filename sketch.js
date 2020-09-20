66243846aaaac34e1bba1cfe1686c842972324e3
Kshitij31-png <71384580+Kshitij31-png@users.noreply.github.com>
2020-09-20 22:31:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var speed,weight;
+var car,wall;
+function setup() {
+  createCanvas(1600,400);
+
+  car = createSprite(50,50,40,10);
+  wall = createSprite(1500,50,20,height/10);
+  wall.shapeColor = "red";
+  speed = random(55,90);
+  weight = random(400,1500);
+  car.velocityX = speed;
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+
+  if(wall.x-car.x < (car.width+wall.width)/2)
+  {
+    car.velocityX = 0;  
+    var deformation = 0.5*weight*speed*speed/22500;
+
+
+  if(deformation>180)
+  {
+    car.shapeColor = color(255,0,0);
+    console.log("Red");
+  }
+  
+  if(deformation<180 && deformation>120)
+  {
+    car.shapeColor = color(230,230,0);
+    console.log("yellow");
+
+  }
+
+  if(deformation<120)
+  {
+    car.shapeColor = color(0,255,0);
+    console.log("green");
+  }
+ }
+}
